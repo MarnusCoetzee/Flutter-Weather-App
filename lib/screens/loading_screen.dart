0bd73a9bf840d5227c3e85dc7bb88ce210b72bbb
Marnus Coetzee <marnuscoe@gmail.com>
2020-08-05 11:40:10 +0200

    finished project

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,22 +1,16 @@
-import 'package:clima/screens/location_screen.dart';
-import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:clima/services/location.dart';
-import 'package:clima/keys.dart';
+import 'package:clima/screens/location_screen.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
-  _LoadingScreenState createState() => _LoadingScreenState();
+  State<StatefulWidget> createState() {
+    return _LoadingScreenState();
+  }
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
-  static const key = apiKey;
-
-  double latitude;
-  double longitude;
-
   @override
   void initState() {
     super.initState();
@@ -24,26 +18,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-    latitude = location.latitude;
-    longitude = location.longitude;
-
-    NetworkHelper networkHelper = NetworkHelper(url: 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$key');
-
-    var weatherData = await networkHelper.getData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return LocationScreen(locationWeather: weatherData);
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
     }));
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SpinKitFoldingCube(
+        child: SpinKitDoubleBounce(
           color: Colors.white,
           size: 100.0,
         ),
--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,24 +1,20 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:clima/keys.dart';
-
-const key = apiKey;
 
 class NetworkHelper {
-
-  NetworkHelper({ this.url });
+  NetworkHelper(this.url);
 
   final String url;
 
   Future getData() async {
     http.Response response = await http.get(url);
+
     if (response.statusCode == 200) {
       String data = response.body;
-      var decodedData = jsonDecode(data);
-      return decodedData;
+
+      return jsonDecode(data);
     } else {
-      print(response.body);
+      print(response.statusCode);
     }
   }
-
 }
